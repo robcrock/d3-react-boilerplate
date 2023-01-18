c36f56fc76e83c31c55a94d42eea9ef59c291e92
robcrockbb <robert@vizsimply.com>
2023-01-18 14:28:21 -0300

    Added the types package for D3 and logged the result of the d3.scaleLinear method.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import * as d3 from "d3"
+
 function App() {
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  const scale = d3.scaleLinear().domain([0, 10]).range(data)
+
+  console.log("Scale", scale(100))
+
   return <div className="App"></div>
 }
 
