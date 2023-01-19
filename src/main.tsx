43b62a379f332d3e568bbb8df1329669789fa82c
robcrockbb <robert@vizsimply.com>
2023-01-19 14:23:16 -0300

    Added React Query and React Query DevTools.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,22 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 
+// Using Chakra UI
 import { ChakraProvider } from "@chakra-ui/react"
+
+// Using React Query
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider resetCSS={false}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} position={"bottom-right"} />
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 )
